eb7d127d0f440a6079db7e5e9022d631d1bc6e9c
Kaseaa <>
2020-06-11 01:17:18 +0200

    updated defs

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 module.exports = function SwearWords(dispatch) {
-	for(let msg of ['S_CHAT', 'S_WHISPER', 'S_PRIVATE_CHAT', 'C_CHAT', 'C_WHISPER']) dispatch.hook(msg, 1, {order: 100}, unfilter);
+	for(let msg of ['S_CHAT', 'S_WHISPER', 'S_PRIVATE_CHAT', 'C_CHAT', 'C_WHISPER']) {
+		if(["S_CHAT", "S_WHISPER"].includes(msg)) dispatch.hook(msg, 3, {order: 100}, unfilter);
+		else dispatch.hook(msg, 1, {order: 100}, unfilter);
+	}
 }
 
 function unfilter(event) {
