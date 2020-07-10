3b49dc72534b5f14c475ffbfd60fa4703c3145d0
Kaseaa <>
2020-07-10 21:59:33 +0200

    updated

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-module.exports = function SwearWords(dispatch) {
-	for(let msg of ['S_CHAT', 'S_WHISPER', 'S_PRIVATE_CHAT', 'C_CHAT', 'C_WHISPER']) {
-		if(["S_CHAT", "S_WHISPER"].includes(msg)) dispatch.hook(msg, 3, {order: 100}, unfilter);
-		else dispatch.hook(msg, 1, {order: 100}, unfilter);
-	}
-}
+module.exports = function SwearWords(mod) {
+  const unfilter = (e) => {
+    e.message = e.message.replace(/<FONT>(.*?)<\/FONT>/g, "<FONT></FONT>$1");
+    return true;
+  };
 
-function unfilter(event) {
-	event.message = event.message.replace(/<FONT>(.*?)<\/FONT>/g, '<FONT></FONT>$1');
-	return true;
-}
+  for (const data of [
+    ["S_CHAT", 3],
+    ["S_WHISPER", 3],
+    ["S_PRIVATE_CHAT", 1],
+    ["C_CHAT", 1],
+    ["C_WHISPER", 1],
+  ]) {
+    mod.hook(...data, { order: 100 }, unfilter);
+  }
+};
